69a9ea01823f54c5910b16a0ff055727d2f5023a
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-25 17:09:15 +0100

    Adds demonstration 10, for Collections (ArrayList)

---
+++ D10_Colecao_ArrayList/Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10_Colecao_ArrayList
+{
+
+    // Base class
+    internal class Pessoa
+    {
+
+        #region Properties
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        #endregion
+
+        #region Constructors (no mínimo 2)
+
+        public Pessoa()
+        {
+
+            Id = 0;
+            Nome = string.Empty;
+
+        }
+
+        public Pessoa(int id, string nome)
+        {
+
+            Id = id;
+            Nome = nome;
+
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+
+    }
+
+}
