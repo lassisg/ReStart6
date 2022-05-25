867e79d3c5fa4e14a5ccba55639f281f3db439c1
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-25 15:59:56 +0100

    Adds code for Data Types, from class 4

---
+++ D03_DataTypes/Curso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D03_DataTypes
+{
+
+    class Curso
+    {
+
+        // Class elements - methods, properties, events
+
+        #region Fields (devem ser privados)
+
+        string nome;
+
+        #endregion
+
+        #region Properties (devem ser publicas)
+
+        public int CursoID { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public double numeroHoras { get; set; }
+
+        #endregion
+
+    }
+
+}
