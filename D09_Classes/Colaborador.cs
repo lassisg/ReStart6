f1ec74cf9612fc4bab4346111284cd76ece2613a
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-25 16:40:02 +0100

    Adds demonstration 9 for Classes

---
+++ D09_Classes/Colaborador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D09_Classes
+{
+
+    internal class Colaborador
+    {
+
+        #region Fields: variáveis privadas à classe que suportam as propriedades clássicas
+
+        private string nome;
+
+        #endregion
+
+        #region Properties
+
+        // Propriedades auto-implemented
+        public int ColaboradorID { get; set; }
+        public string Email { get; set; }
+        public string Localidade { get; set; }
+
+        // Propriedades clássicas
+        public string Nome          // usa-se o correspondente field
+        { 
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        // Expression bodied: Avançado, não utilizaremos
+        // public string LastName => "Nome";
+
+        #endregion
+
+        #region Constructors
+
+        public Colaborador()
+        {
+            ColaboradorID = 0;
+            Nome = "";
+            Email = "";
+            Localidade = string.Empty;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ D09_Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D09_Classes
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Colaborador c = new Colaborador();
+            Console.WriteLine(c.Nome);
+
+            // sets
+            c.ColaboradorID = 1;
+            c.Nome = "L";
+
+            // gets
+            Console.WriteLine(c.ColaboradorID);
+            Console.WriteLine(c.Nome);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
