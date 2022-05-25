2a7731e83bc3962d60c43f7b908ea29d5d06315e
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-25 16:50:14 +0100

    Adds utility project

---
+++ D00_Utils/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace D00_Utils
+{
+
+    public class Utils
+    {
+
+        public static void PrintHeader(string title, string newLines = "")
+        {
+
+            string border = new string ('-', 70);
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(newLines);
+            sb.AppendLine(border);
+            sb.AppendLine(title.ToUpper());
+            sb.AppendLine(border);
+
+            Console.Clear();
+            Console.Write(sb.ToString());
+
+        }
+
+        public static void PrintSubHeader(string subTitle)
+        {
+
+            Console.WriteLine($"\n{subTitle}");
+            Console.WriteLine(new string('-', 43));
+
+        }
+
+        public static void CleanConsole()
+        {
+
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+
+    }
+
+}
