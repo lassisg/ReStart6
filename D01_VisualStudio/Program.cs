7f78f5531551f1858aaf5818fa0648482ec07bc2
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-25 14:12:36 +0100

    Adds code from 1st class

---
+++ D01_VisualStudio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace D01_VisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Program.cs > Main
+                Entry point
+            */
+            Console.Write("Hello C#");
+            Console.Read();
+        }
+    }
+}
