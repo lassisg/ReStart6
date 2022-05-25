1bc935173d095f06e26cd226d04802c97373ec85
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-25 17:22:24 +0100

    Adds demonstration 13 for Generic Collection (Stack)

---
+++ D13_ColecaoGeneric_Stack/Program.cs
@@ -0,0 +1,60 @@
+﻿using D00_Utils;
+using System;
+using System.Collections.Generic;
+
+namespace D13_ColecaoGeneric_Stack
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Utils.SetUTF8Encoding();
+
+            #region Stack
+
+            Utils.PrintHeader("Stack");
+
+            // Criar ums Stack de inteiros
+            Stack<int> pilhaInteiros = new Stack<int>();
+
+            // Adicionar 1 a 1
+            pilhaInteiros.Push(1);
+            pilhaInteiros.Push(2);
+            pilhaInteiros.Push(3);
+
+            // Adicionar 3 valores, duma só vez
+            for (int i = 4; i < 7; i++)
+            {
+                pilhaInteiros.Push(i);
+            }
+
+            Console.WriteLine("pilhaInteiros antes do Dequeuing:");
+            foreach (int item in pilhaInteiros)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Eliminar itens da lista e mostrar o item eliminado
+            Console.WriteLine("\nPoping:");
+            Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 6
+            Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 5
+            Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 4
+
+            Console.WriteLine("\npilhaInteiros após o Dequeuing:");
+            foreach (int item in pilhaInteiros)
+            {
+                Console.WriteLine(item);
+            }
+
+            Utils.CleanConsole();
+
+            #endregion
+
+        }
+
+    }
+
+}
