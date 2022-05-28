df38ae2dfa3c9a98a53f14de7ce8ae56033e8c12
Leandro Gabriel <leandro@bigiogabriel.com>
2022-05-28 17:36:18 +0100

    Adds project for exercises from Module 8 (Vectors & Matrices)

---
+++ M08_VectorsMatrices/Exercicios.cs
@@ -0,0 +1,130 @@
+﻿using D00_Utils;
+
+namespace M08_VectorsMatrices
+{
+    internal class Exercicios
+    {
+
+        internal static void ExecutarExercicio01()
+        {
+
+            Utils.PrintHeader("Exercício 1");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever um programa que permita criar um vetor com comprimento 10.";
+            subHeader += "\nDepois deve de alterar o valor dos índices 3 e 9.No final deve de";
+            subHeader += "\nmostrar a evolução desta troca.";
+
+            Utils.PrintSubHeader(subHeader);
+
+
+        }
+
+        internal static void ExecutarExercicio02()
+        {
+
+            Utils.PrintHeader("Exercício 2");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever um programa para calcular a multiplicação, soma e média de";
+            subHeader += "\ntodos os elementos de um vetor com comprimento de 7.";
+
+            Utils.PrintSubHeader(subHeader);
+
+
+
+        }
+
+        internal static void ExecutarExercicio03()
+        {
+
+            Utils.PrintHeader("Exercício 3");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever um programa para encontrar o índice e o valor de maior valor.";
+
+            Utils.PrintSubHeader(subHeader);
+
+        }
+
+        internal static void ExecutarExercicio04()
+        {
+
+            Utils.PrintHeader("Exercício 4");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever um programa que permita criar um vetor com comprimento 8 e";
+            subHeader += "\nque recorrendo ao ciclo foreach passe o valor de cada índice para";
+            subHeader += "\numa variável inteira.";
+
+            Utils.PrintSubHeader(subHeader);
+
+            
+
+        }
+
+        internal static void ExecutarExercicio05()
+        {
+
+            Utils.PrintHeader("Exercício 5");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever um programa que leia N números reais para um vetor com o";
+            subHeader += "\nmáximo de 100 números e apresente no ecrã a soma dos números.";
+
+            Utils.PrintSubHeader(subHeader);
+
+
+
+        }
+
+        internal static void ExecutarExercicio06()
+        {
+
+            Utils.PrintHeader("Exercício 6");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever e testar um método que pesquise num vetor de inteiros V um";
+            subHeader += "\ndeterminado número inteiro N e devolva o número de vezes que encontrou";
+            subHeader += "\nesse número. Se não encontrar deverá devolver - 1";
+
+            Utils.PrintSubHeader(subHeader);
+
+
+
+        }
+
+        internal static void ExecutarExercicio07()
+        {
+
+            Utils.PrintHeader("Exercício 7");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escrever e testar um método para verificar se dois vetores de inteiros";
+            subHeader += "\nsão iguais. Dois vetores são iguais se na mesma posição tiverem";
+            subHeader += "\nelementos com o mesmo valor.";
+
+            Utils.PrintSubHeader(subHeader);
+
+            
+
+        }
+
+        internal static void ExecutarExercicio08()
+        {
+
+            Utils.PrintHeader("Exercício 8");
+            string subHeader;
+            // ----------------------------------------------------------------------
+            subHeader = "Escreva um programa que leia e apresente um conjunto de números";
+            subHeader += "\ninteiros para uma matriz bidimensional 4 x 3";
+
+            Utils.PrintSubHeader(subHeader);
+
+
+
+        }
+
+    }
+
+}
