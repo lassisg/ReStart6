678575187ea3243869e6947d333fcd16fedd4414
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-04 16:01:26 +0100

    Adds request properties and constructors

--- RSGymPT/Request.cs
+++ RSGymPT/Request.cs
@@ -10,7 +10,53 @@ namespace RSGymPT
     internal class Request
     {
 
+        #region Enums
 
+        internal enum EnumStatus
+        {
+            None,
+            Active,
+            Completed,
+            Canceled
+        }
+
+        #endregion
+
+        #region Properties
+
+        internal int Id { get; set; }
+        internal string RequestNumber { get; set; }
+        internal DateTime RequestDate { get; set; }
+        public string TrainerName { get; set; }
+        public EnumStatus RequestStatus { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        internal Request()
+        {
+            RequestNumber = string.Empty;
+            RequestDate = DateTime.MinValue;
+            TrainerName = string.Empty;
+            RequestStatus = EnumStatus.None;
+        }
+
+        internal Request(string requestNumber, DateTime requestDate, string trainerName, EnumStatus requestStatus)
+        {
+            RequestNumber = requestNumber;
+            RequestDate = requestDate;
+            TrainerName = trainerName;
+            RequestStatus = requestStatus;
+        }
+
+        #endregion
+
+        #region Methods
+
+        
+
+        #endregion
 
     }
 
