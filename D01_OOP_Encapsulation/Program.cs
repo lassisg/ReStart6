6dd141bf50ac084b91a1d28ce0047e71d14ca743
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-06 20:10:42 +0100

    Adds course instance

--- D01_OOP_Encapsulation/Program.cs
+++ D01_OOP_Encapsulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D00_Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,19 @@ namespace D01_OOP_Encapsulation
         static void Main(string[] args)
         {
 
+            Utils.SetUTF8Encoding();
 
+            Utils.PrintHeader("Encapsulation");
+
+            // Instanciar a classe e usar os seus membros públicos
+            Course course01 = new Course();
+
+
+            course01.CourseID = 1;
+            course01.CourseName = "C#";
+            course01.ListCourse();
+
+            Utils.CleanConsole();
 
         }
 
