99193460d61e099ed3270d5aba572e2e7972a8a7
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-06 22:09:04 +0100

    Adds class for polymorphism demonstration

---
+++ D03_OOP_Polymorphism/CourseSoftSkills.cs
@@ -0,0 +1,31 @@
+﻿namespace D03_OOP_Polymorphism
+{
+
+    // Inheritance
+    internal class CourseSoftSkills : Course
+    {
+
+        #region Properties
+
+        internal string Area { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        // Mapear os construtores desta classe para os da classe base
+        internal CourseSoftSkills() : base()
+        {
+            Area = string.Empty;
+        }
+
+        internal CourseSoftSkills(int courseID, string courseName, string area) : base(courseID, courseName)
+        {
+            Area = area;
+        }
+
+        #endregion
+
+    }
+
+}
