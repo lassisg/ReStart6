cd5a8404f02060017586f760f08deed4f6f94ce2
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-06 19:22:49 +0100

    Add project files.

---
+++ D00_Utils/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace D00_Utils
+{
+
+    public class Utils
+    {
+
+        public static void SetUTF8Encoding()
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        }
+
+        public static void PrintHeader(string title, string newLines = "")
+        {
+
+            string border = new string ('-', 70);
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(newLines);
+            sb.AppendLine(border);
+            sb.AppendLine(title.ToUpper());
+            sb.AppendLine(border);
+
+            Console.Clear();
+            Console.Write(sb.ToString());
+
+        }
+
+        public static void PrintSubHeader(string subTitle)
+        {
+
+            Console.WriteLine($"\n{subTitle}");
+            Console.WriteLine(new string('-', 43));
+
+        }
+
+        public static void CleanConsole()
+        {
+
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+
+        public static void PrintArrayList(ArrayList arrayList)
+        {
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+
+}
