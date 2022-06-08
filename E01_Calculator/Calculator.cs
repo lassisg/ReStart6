ad103466ba35924f010ca04f979cb2f9102e2f55
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-08 19:14:27 +0100

    Adds abstract class Calculator

---
+++ E01_Calculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_Calculator
+{
+
+    internal abstract class Calculator
+    {
+
+
+
+    }
+
+}
