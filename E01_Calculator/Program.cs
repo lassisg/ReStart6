1f063905dbf6b65b656fe5690cfd89b5af7ca81e
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-08 19:14:14 +0100

    Adds a project for exercise 1 (Calculator)

---
+++ E01_Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_Calculator
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+    }
+
+}
