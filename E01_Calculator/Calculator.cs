764032a63a842d606240a67133dff683aa6b6c26
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-09 20:17:28 +0100

    COncludes calculator exercise v1, with minor issues

--- E01_Calculator/Calculator.cs
+++ E01_Calculator/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D00_Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,59 @@ namespace E01_Calculator
     internal abstract class Calculator
     {
 
+        #region Properties
 
+        internal abstract double Result { get; set; }
+        internal abstract double Value01 { get; set; }
+        internal abstract double Value02 { get; set; }
+
+        #endregion
+
+        #region Contructors (Classe abstrata, não será instanciada. Logo, não deve ter construtores)
+
+        //internal Calculator()
+        //{
+        //    Value01 = 0;
+        //    Value02 = 0;
+        //    Result = 0;
+        //}
+
+        //internal Calculator(double value01, double value02)
+        //{
+        //    Value01 = value01;
+        //    Value02 = value02;
+        //    Result = 0;
+        //}
+
+        #endregion
+
+        #region Methods
+
+        internal abstract void ShowMenu();
+        
+        internal abstract string ReadUserInput();
+
+        internal abstract bool ValidateOption(string selectedOption);
+
+        internal abstract void ReadNumbers(string inputString);
+
+        internal abstract double ValidateNumber(string inputString);
+
+        internal abstract void ExecuteOperation(string selectedOperation);
+
+        internal abstract void ShowResult();
+
+        internal abstract double Sum(double value01, double value02);
+        
+        internal abstract double Subtract(double value01, double value02);
+        
+        internal abstract double Multiply(double value01, double value02);
+        
+        internal abstract double Divide(double value01, double value02);
+
+        internal abstract void WriteErrorMessage(string message);
+        
+        #endregion
 
     }
 
--- E01_Calculator/Program.cs
+++ E01_Calculator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D00_Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,101 @@ namespace E01_Calculator
         static void Main(string[] args)
         {
 
+            #region Variables
 
+            Calculator calculadora;
+
+            bool isExit = false;
+            string errorMessage;
+
+            #endregion
+
+            #region Methods
+
+            do
+            {
+                errorMessage = string.Empty;
+
+                Console.WriteLine("Que tipo de calculadora deseja utilizar?");
+                Console.WriteLine("1 - Calculadora padrão");
+                Console.WriteLine("2 - Calculadora cinetífica");
+                Console.WriteLine("x - Sair");
+
+                string selectedCalculator = Console.ReadLine();
+                try
+                {
+
+                    switch (selectedCalculator)
+                    {
+                        case "1":
+                            calculadora = new CalculatorStandard();
+                            break;
+
+                        case "2":
+                            calculadora = new CalculatorScientific();
+                            break;
+
+                        case "x":
+                            calculadora = null;
+                            isExit = true;
+                            break;
+
+                        default:
+                            throw new InvalidOperationException("Opção inválida!.");
+                    }
+
+                    if (!isExit)
+                    {
+                        do
+                        {
+                            calculadora.ShowMenu();
+
+                            string selectedOption = calculadora.ReadUserInput();
+                            bool isValidOption = calculadora.ValidateOption(selectedOption);
+
+                            if (!isValidOption)
+                            {
+                                calculadora.WriteErrorMessage("Opção inválida!");
+                                continue;
+                            }
+
+                            if (selectedOption == "x")
+                                break;
+
+                            calculadora.ReadNumbers(selectedOption);
+                            calculadora.ExecuteOperation(selectedOption);
+                            calculadora.ShowResult();
+                            Console.ReadLine();
+
+                        } while (!isExit);
+                    }
+
+                }
+                catch (InvalidOperationException e)
+                {
+                    errorMessage = e.Message;
+                }
+                catch (Exception e)
+                {
+                    errorMessage = e.Message;
+                }
+                finally
+                {
+                    if (errorMessage != string.Empty)
+                    {
+                        string whiteSpace = new string(' ', 3);
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"\n<{whiteSpace}{errorMessage}{whiteSpace}>\n");
+                        Console.ResetColor();
+                        Console.ReadKey();
+                    }
+                    Console.Clear();
+                }
+
+            } while (!isExit);
+
+            #endregion
 
         }
 
