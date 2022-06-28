dfe30299814265be0026a46c2190b971c5675cf3
Leandro Gabriel <leandro@bigiogabriel.com>
2022-06-28 07:05:52 +0100

    Adds interfaces for better SOLID principles use

---
+++ RSGymPT/Commands/IBaseCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RSGymPT
+{
+
+    internal interface IBaseCommand
+    {
+        string Name { get; }
+
+        string HelpText { get; }
+
+        bool IsPrivileged { get; }
+
+        Dictionary<string, string> Arguments { get; }
+
+        string Pattern { get; }
+
+    }
+
+}
---
+++ RSGymPT/Commands/IRequestCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RSGymPT
+{
+
+    internal interface IRequestCommand : IBaseCommand
+    {
+
+        bool Execute(string args);
+
+        bool IsValid(string args);
+
+        string GetHelp();
+
+    }
+
+}
