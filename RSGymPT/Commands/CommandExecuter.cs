17c4fc4d8c6347a9ada44a39ac7d648d3341c8fe
Leandro Gabriel <leandro@bigiogabriel.com>
2022-07-05 15:24:30 +0100

    Adds interfaces to improve overall code.

---
+++ RSGymPT/Commands/CommandExecuter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSGymPT
+{
+    internal class CommandExecuter : IExecuter
+    {
+
+        #region Parameters
+
+        public ICommand Command { get; set; }
+        
+        public string InputCommand { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        internal CommandExecuter(ICommand command, string inputCommand)
+        {
+            Command = command;
+            InputCommand = inputCommand;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public CommandExecuter ValidateCommandStructure()
+        {
+            Command.IsValid = Command.ValidateStructure(InputCommand);
+            return this;
+        }
+
+        public CommandExecuter ValidateArgumentValues()
+        {
+            bool hasValidArguments = Command.ValidateArguments(InputCommand);
+            return this;
+        }
+
+        public CommandExecuter ExecuteCommand()
+        {
+            bool success = Command.Execute();
+            return this;
+        }
+
+        public bool IsExitCommand()
+        {
+            bool isExit = Command.GetType() == typeof(ExitCommand);
+            return isExit;
+        }
+
+        #endregion
+
+    }
+
+}
