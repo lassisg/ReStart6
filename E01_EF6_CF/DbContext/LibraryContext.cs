e4f1edcb484d19576ba075ef5af8aeee39394221
Leandro Gabriel <leandro@bigiogabriel.com>
2022-07-12 17:31:01 +0100

    Adds project for exercise 01

---
+++ E01_EF6_CF/DbContext/LibraryContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace E01_EF6_CF
+{
+
+    internal class LibraryContext : DbContext
+    {
+
+        public LibraryContext()
+            : base("name=LibraryContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Desativar a pluralidade das tabelas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+
+}
