9c267932cf3ad648d10e83928b88b9319d1a3cf2
Leandro Gabriel <leandro@bigiogabriel.com>
2022-07-12 19:41:38 +0100

    Moves Exercise 1 to its own solution

---
+++ E01_EF6_CF/Class/Publisher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_EF6_CF
+{
+
+    internal class Publisher
+    {
+
+        #region Scalar properties
+
+        public int PublisherId { get; set; }
+        public string Name { get; set; }
+        
+        #endregion
+
+    }
+
+}
