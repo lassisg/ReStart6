7f88d6d88eb343de4daa38eb7a970678f6c87cb0
Leandro Gabriel <leandro@bigiogabriel.com>
2022-07-18 20:15:52 +0100

    Add project files.

---
+++ E02_EF6_CF_Migrations_Books_DAL/Model/Publisher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace E02_EF6_CF_Migrations_Books_DAL
+{
+
+    public class Publisher
+    {
+
+        #region Scalar properties
+
+        public int PublisherID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "100 character limit.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Navigation properties
+
+        // 1 publisher - n books
+        public ICollection<Book> Book { get; set; }
+
+        #endregion
+
+    }
+
+}
