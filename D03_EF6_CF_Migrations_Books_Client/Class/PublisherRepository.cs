3c95ab82d37fef95a7981d08900c53c791259f01
Leandro Gabriel <leandro@bigiogabriel.com>
2022-07-20 18:28:29 +0100

    Adds instructor's projects for Migration exercise

---
+++ D03_EF6_CF_Migrations_Books_Client/Class/PublisherRepository.cs
@@ -0,0 +1,86 @@
+﻿using D03_EF6_CF_Migrations_Books_DAL;
+using System;
+using System.Linq;
+
+namespace D03_EF6_CF_Migrations_Books_Client
+{
+
+    // -------
+    // CRUD
+    // -------
+
+    static class PublisherRepository
+    {
+
+        public static void CreatePublisher()
+        {
+
+            var publisher = new Publisher
+            {
+                Name = "New publisher"
+            };
+
+            using (var context = new BooksDBContext())
+            {
+                context.Publisher.Add(publisher);
+                context.SaveChanges();
+
+            }
+
+        }
+
+        public static void ReadPublisher()
+        {
+
+            using (var context = new BooksDBContext())
+            {
+
+                var query = context.Publisher.Select(p => p).OrderBy(p => p.PublisherID);
+
+                Utility.WriteSubtitle("Publishers", "\n", "\n-----------------------");
+
+                query.ToList().ForEach(p => Console.WriteLine($"{p.PublisherID} - {p.Name}"));
+
+            }
+
+        }
+
+        public static void UpdatePublisher()
+        {
+
+            using (var context = new BooksDBContext())
+            {
+
+                var result = context.Publisher.SingleOrDefault(p => p.PublisherID == 2);
+
+                if (result != null)
+                {
+                    result.Name = "Updated publisher";
+                    context.SaveChanges();
+                }
+
+            }
+
+        }
+
+        public static void DeletePublisher()
+        {
+
+            using (var context = new BooksDBContext())
+            {
+
+                var result = context.Publisher.SingleOrDefault(p => p.PublisherID == 3);
+
+                if (result != null)
+                {
+                    context.Publisher.Remove(result);   // Eliminação em cascata: de 1 para n
+                    context.SaveChanges();
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ D03_EF6_CF_Migrations_Books_DAL/Model/Book.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D03_EF6_CF_Migrations_Books_DAL
+{
+
+    public class Book
+    {
+
+        #region Scalar properties
+        public int BookID { get; set; }
+
+        // Foreign key, without data annotation, just using code conventions
+        public int PublisherID { get; set; }
+        
+        [Required]
+        [StringLength(100, ErrorMessage = "100 character limit.")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(9, ErrorMessage = "9 character limit.")]
+        [MaxLength(9)]
+        public string ISBN { get; set; }
+
+        /*
+        [Required]
+        [StringLength(100, ErrorMessage = "100 character limit.")]
+        [MaxLength(100)]
+        public string DDC { get; set; }     // Dewey Decimal Classification
+        */
+        #endregion
+
+        #region Navigation properties
+        // 1 book - n publishers
+        public Publisher Publisher { get; set; }
+        #endregion
+
+    }
+
+}
