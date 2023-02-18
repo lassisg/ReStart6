9dfe08ab2ed6ff9eadfce10a7646c725472abf81
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 10:32:51 +0000

    Update project D13.

--- D13_ColecaoGeneric_Stack/Program.cs
+++ D13_ColecaoGeneric_Stack/Program.cs
@@ -1,60 +1,43 @@
 ﻿using D00_Utils;
-using System;
-using System.Collections.Generic;
 
-namespace D13_ColecaoGeneric_Stack
+Utils.SetUtf8Encoding();
+
+#region Stack
+
+Utils.PrintHeader("Stack");
+
+// Criar ums Stack de inteiros
+Stack<int> pilhaInteiros = new Stack<int>();
+
+// Adicionar 1 a 1
+pilhaInteiros.Push(1);
+pilhaInteiros.Push(2);
+pilhaInteiros.Push(3);
+
+// Adicionar 3 valores, duma só vez
+for (int i = 4; i < 7; i++)
 {
-
-    internal class Program
-    {
-
-        static void Main(string[] args)
-        {
-
-            Utils4.SetUTF8Encoding();
-
-            #region Stack
-
-            Utils4.PrintHeader("Stack");
-
-            // Criar ums Stack de inteiros
-            Stack<int> pilhaInteiros = new Stack<int>();
-
-            // Adicionar 1 a 1
-            pilhaInteiros.Push(1);
-            pilhaInteiros.Push(2);
-            pilhaInteiros.Push(3);
-
-            // Adicionar 3 valores, duma só vez
-            for (int i = 4; i < 7; i++)
-            {
-                pilhaInteiros.Push(i);
-            }
-
-            Console.WriteLine("pilhaInteiros antes do Dequeuing:");
-            foreach (int item in pilhaInteiros)
-            {
-                Console.WriteLine(item);
-            }
-
-            // Eliminar itens da lista e mostrar o item eliminado
-            Console.WriteLine("\nPoping:");
-            Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 6
-            Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 5
-            Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 4
-
-            Console.WriteLine("\npilhaInteiros após o Dequeuing:");
-            foreach (int item in pilhaInteiros)
-            {
-                Console.WriteLine(item);
-            }
-
-            Utils4.CleanConsole();
-
-            #endregion
-
-        }
-
-    }
-
+    pilhaInteiros.Push(i);
 }
+
+Console.WriteLine("pilhaInteiros antes do Dequeuing:");
+foreach (int item in pilhaInteiros)
+{
+    Console.WriteLine(item);
+}
+
+// Eliminar itens da lista e mostrar o item eliminado
+Console.WriteLine("\nPoping:");
+Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 6
+Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 5
+Console.WriteLine($"Poping {pilhaInteiros.Pop()}");   // 4
+
+Console.WriteLine("\npilhaInteiros após o Dequeuing:");
+foreach (int item in pilhaInteiros)
+{
+    Console.WriteLine(item);
+}
+
+Utils.CleanConsole();
+
+#endregion
