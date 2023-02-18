0eb16a472dcefb8556f183e09639c4f0bb188ec2
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 11:17:13 +0000

    Update project M05.

--- M05_InstrucoesDecisao/Program.cs
+++ M05_InstrucoesDecisao/Program.cs
@@ -1,33 +1,19 @@
 ﻿using D00_Utils;
+using M05_InstrucoesDecisao;
 
-namespace M05_InstrucoesDecisao
-{
+Utils.SetUtf8Encoding();
 
-    internal class Program
-    {
-
-        static void Main(string[] args)
-        {
-
-            Utils4.SetUTF8Encoding();
-
-            //Exercicios.ExecutarExercicio01();
-            //Exercicios.ExecutarExercicio02();
-            //Exercicios.ExecutarExercicio03();
-            //Exercicios.ExecutarExercicio04();
-            //Exercicios.ExecutarExercicio05();
-            //Exercicios.ExecutarExercicio06();
-            //Exercicios.ExecutarExercicio07();
-            //Exercicios.ExecutarExercicio08();
-            //Exercicios.ExecutarExercicio09();
-            //Exercicios.ExecutarExercicio10();
-            //Exercicios.ExecutarExercicio11();
-            //Exercicios.ExecutarExercicio12();
-            //Exercicios.ExecutarExercicio13();
-            Exercicios.ExecutarExercicio14();
-
-        }
-
-    }
-
-}
+//Exercicios.ExecutarExercicio01();
+//Exercicios.ExecutarExercicio02();
+//Exercicios.ExecutarExercicio03();
+//Exercicios.ExecutarExercicio04();
+//Exercicios.ExecutarExercicio05();
+//Exercicios.ExecutarExercicio06();
+//Exercicios.ExecutarExercicio07();
+//Exercicios.ExecutarExercicio08();
+//Exercicios.ExecutarExercicio09();
+//Exercicios.ExecutarExercicio10();
+//Exercicios.ExecutarExercicio11();
+//Exercicios.ExecutarExercicio12();
+//Exercicios.ExecutarExercicio13();
+Exercicios.ExecutarExercicio14();
--- M05_InstrucoesDecisao/Triangle.cs
+++ M05_InstrucoesDecisao/Triangle.cs
@@ -1,83 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace M05_InstrucoesDecisao;
 
-namespace M05_InstrucoesDecisao
+internal class Triangle
 {
 
-    internal class Triangle
+    #region Properties
+
+    public int SideA { get; set; }
+    public int SideB { get; set; }
+    public int SideC { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public Triangle()
     {
 
-        #region Properties
-
-        public int SideA { get; set; }
-        public int SideB { get; set; }
-        public int SideC { get; set; }
-
-        #endregion
-
-        #region Constructors
-
-        public Triangle()
-        {
-
-        }
-
-        public Triangle(int[] sides)
-        {
-            SideA = Convert.ToInt16(sides[0]);
-            SideB = Convert.ToInt16(sides[1]);
-            SideC = Convert.ToInt16(sides[2]);
-        }
-
-        public Triangle(int sideA, int sideB, int sideC)
-        {
-            this.SideA = sideA;
-            this.SideB = sideB;
-            this.SideC = sideC;
-        }
-
-        #endregion
-
-        #region Methods
-
-        public bool isTriangle()
-        {
-            bool isTriangle = false;
-
-            if ((SideA + SideB > SideC) & (SideA + SideC > SideB) & (SideB + SideC > SideA))
-            {
-                isTriangle = true;
-            }
-
-            return isTriangle;
-        }
-
-        public string GetTriangleType()
-        {
-
-            string trinagleType;
-
-            if ((SideA == SideB) & (SideA == SideC))
-            {
-                trinagleType = "equilátero";
-            }
-            else if ((SideA == SideB) | (SideA == SideC) | (SideB == SideC))
-            {
-                trinagleType = "isósceles";
-            }
-            else
-            {
-                trinagleType = "escaleno";
-            }
-
-            return trinagleType;
-        }
-
-        #endregion
-
     }
+
+    public Triangle(int[] sides)
+    {
+        SideA = Convert.ToInt16(sides[0]);
+        SideB = Convert.ToInt16(sides[1]);
+        SideC = Convert.ToInt16(sides[2]);
+    }
+
+    public Triangle(int sideA, int sideB, int sideC)
+    {
+        this.SideA = sideA;
+        this.SideB = sideB;
+        this.SideC = sideC;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public bool isTriangle()
+    {
+        bool isTriangle = false;
+
+        if ((SideA + SideB > SideC) & (SideA + SideC > SideB) & (SideB + SideC > SideA))
+        {
+            isTriangle = true;
+        }
+
+        return isTriangle;
+    }
+
+    public string GetTriangleType()
+    {
+
+        string trinagleType;
+
+        if ((SideA == SideB) & (SideA == SideC))
+        {
+            trinagleType = "equilátero";
+        }
+        else if ((SideA == SideB) | (SideA == SideC) | (SideB == SideC))
+        {
+            trinagleType = "isósceles";
+        }
+        else
+        {
+            trinagleType = "escaleno";
+        }
+
+        return trinagleType;
+    }
+
+    #endregion
 
 }
