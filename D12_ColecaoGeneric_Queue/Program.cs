48c2766104ac8347335ca1a0e822ece7657a6df1
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 10:31:11 +0000

    Update project D12.

--- D12_ColecaoGeneric_Queue/Program.cs
+++ D12_ColecaoGeneric_Queue/Program.cs
@@ -1,60 +1,43 @@
 ﻿using D00_Utils;
-using System;
-using System.Collections.Generic;
 
-namespace D12_ColecaoGeneric_Queue
+Utils.SetUtf8Encoding();
+
+#region Queue
+
+Utils.PrintHeader("Queue");
+
+// Criar um Queue de inteiros
+Queue<int> filaInteiros = new Queue<int>();
+
+// Adicionar 1 a 1
+filaInteiros.Enqueue(1);
+filaInteiros.Enqueue(2);
+filaInteiros.Enqueue(3);
+
+// Adicionar 3 valores, duma só vez
+for (int i = 4; i < 7; i++)
 {
-
-    internal class Program
-    {
-
-        static void Main(string[] args)
-        {
-
-            Utils4.SetUTF8Encoding();
-
-            #region Queue
-
-            Utils4.PrintHeader("Queue");
-
-            // Criar um Queue de inteiros
-            Queue<int> filaInteiros = new Queue<int>();
-
-            // Adicionar 1 a 1
-            filaInteiros.Enqueue(1);
-            filaInteiros.Enqueue(2);
-            filaInteiros.Enqueue(3);
-
-            // Adicionar 3 valores, duma só vez
-            for (int i = 4; i < 7; i++)
-            {
-                filaInteiros.Enqueue(i);
-            }
-
-            Console.WriteLine("filaInteiros antes do Dequeuing:");
-            foreach (int item in filaInteiros)
-            {
-                Console.WriteLine(item);
-            }
-
-            // Eliminar itens da lista e mostrar o item eliminado
-            Console.WriteLine("\nDequeuing:");
-            Console.WriteLine($"Dequeuing {filaInteiros.Dequeue()}");   // 1
-            Console.WriteLine($"Dequeuing {filaInteiros.Dequeue()}");   // 2
-            Console.WriteLine($"Dequeuing {filaInteiros.Dequeue()}");   // 3
-
-            Console.WriteLine("\nfilaInteiros após o Dequeuing:");
-            foreach (int item in filaInteiros)
-            {
-                Console.WriteLine(item);
-            }
-
-            Utils4.CleanConsole();
-
-            #endregion
-
-        }
-
-    }
-
+    filaInteiros.Enqueue(i);
 }
+
+Console.WriteLine("filaInteiros antes do Dequeuing:");
+foreach (int item in filaInteiros)
+{
+    Console.WriteLine(item);
+}
+
+// Eliminar itens da lista e mostrar o item eliminado
+Console.WriteLine("\nDequeuing:");
+Console.WriteLine($"Dequeuing {filaInteiros.Dequeue()}");   // 1
+Console.WriteLine($"Dequeuing {filaInteiros.Dequeue()}");   // 2
+Console.WriteLine($"Dequeuing {filaInteiros.Dequeue()}");   // 3
+
+Console.WriteLine("\nfilaInteiros após o Dequeuing:");
+foreach (int item in filaInteiros)
+{
+    Console.WriteLine(item);
+}
+
+Utils.CleanConsole();
+
+#endregion
