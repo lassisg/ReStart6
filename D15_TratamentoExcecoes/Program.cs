b84448e9d358d12045e2426ee7aefd23f5aed32d
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 10:36:42 +0000

    Update project D15.

--- D15_TratamentoExcecoes/Program.cs
+++ D15_TratamentoExcecoes/Program.cs
@@ -1,68 +1,52 @@
 ﻿using D00_Utils;
-using System;
 
-namespace D15_TratamentoExcecoes
+Utils.SetUtf8Encoding();
+
+#region Try... Catch...Finally
+
+Utils.PrintHeader("Try... Catch...Finally");
+
+try
 {
 
-    internal class Program
-    {
+    int valor01, valor02;
+    string texto;
 
-        static void Main(string[] args)
-        {
+    valor01 = 10;
+    valor02 = 0;
+    texto = "Erro";
 
-            Utils4.SetUTF8Encoding();
+    Console.WriteLine(10 / 2);
 
-            #region Try... Catch...Finally
+    // Console.WriteLine(10 / 0);   // Erro de sintaxe!!! Mas com variáveis é possível contornar
 
-            Utils4.PrintHeader("Try... Catch...Finally");
+    Console.WriteLine(valor01 * int.Parse(texto));          // System.FormatException
 
-            try
-            {
+    //Console.WriteLine(valor01 * Convert.ToInt16(texto));    // System.FormatException
 
-                int valor01, valor02;
-                string texto;
+    Console.WriteLine(valor01 / valor02);   // Exception: System.DivideByZeroException
 
-                valor01 = 10;
-                valor02 = 0;
-                texto = "Erro";
-
-                Console.WriteLine(10 / 2);
-
-                // Console.WriteLine(10 / 0);   // Erro de sintaxe!!! Mas com variáveis é possível contornar
-
-                Console.WriteLine(valor01 * int.Parse(texto));          // System.FormatException
-
-                //Console.WriteLine(valor01 * Convert.ToInt16(texto));    // System.FormatException
-
-                Console.WriteLine(valor01 / valor02);   // Exception: System.DivideByZeroException
-
-                Console.WriteLine(valor01 * valor02);   // 0
-
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Erro! Formato errado!");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Erro! Divisão por zero!");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Erro!");
-                // throw;    // Útil para saber o tipo de erro
-            }
-            finally
-            {
-                
-                Utils4.CleanConsole();
-
-            }
-
-            #endregion
-
-        }
-
-    }
+    Console.WriteLine(valor01 * valor02);   // 0
 
 }
+catch (FormatException)
+{
+    Console.WriteLine("Erro! Formato errado!");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Erro! Divisão por zero!");
+}
+catch (Exception)
+{
+    Console.WriteLine("Erro!");
+    // throw;    // Útil para saber o tipo de erro
+}
+finally
+{
+                
+    Utils.CleanConsole();
+
+}
+
+#endregion
