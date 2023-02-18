e565a335b753cc094ecf52676f3b03354e6aae29
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 09:43:27 +0000

    Update project D02.

---
+++ D02_Algoritmia/E05_InscricaoCurso.cs
@@ -0,0 +1,57 @@
+﻿namespace D02_Algoritmia;
+
+public static class E05_InscricaoCurso
+{
+
+    public static void InscreverCurso01()
+    {
+
+        bool programador;
+        string curso;
+
+        Console.Write("Sabes programar? (true/false) ");
+        programador = Convert.ToBoolean(Console.ReadLine());
+
+        if (programador)
+        {
+            curso = "C# Foundations";
+        }
+        else
+        {
+            curso = "Programming Fundamentals";
+        }
+
+        Console.WriteLine($"\nAluno inscrito no curso " + curso);
+
+        Console.ReadLine();
+
+    }
+
+    public static void InscreverCurso02()
+    {
+
+        string programador;
+        string curso;
+
+        Console.Write("Sabes programar? (S/N) ");
+        programador = Console.ReadLine();
+
+        if (programador == "S")
+        {
+            curso = "C# Foundations";
+        }
+        else
+        {
+            curso = "Programming Fundamentals";
+        }
+
+        // Console.WriteLine("\nAluno inscrito no curso " + curso);
+
+        // String interpolation
+        Console.WriteLine($"\nAluno inscrito no curso {curso}");
+
+        Console.ReadLine();
+
+    }
+
+}
---
+++ D02_Algoritmia/Program.cs
@@ -0,0 +1,27 @@
+﻿using D02_Algoritmia;
+
+E01_MediaAritmetica.CalcularMediaAritmetica();
+
+Console.Clear();
+
+E02_MaiorIdade.AnalisarMaioridade01();
+
+Console.Clear();
+
+E02_MaiorIdade.AnalisarMaioridade02();
+
+Console.Clear();
+
+E03_MaiorNumero.CalcularMaiorNumero();
+
+Console.Clear();
+
+E04_MaiorIdadeRepeticao.AnalisarMaioridade();
+
+Console.Clear();
+
+E05_InscricaoCurso.InscreverCurso01();
+
+Console.Clear();
+
+E05_InscricaoCurso.InscreverCurso02();
