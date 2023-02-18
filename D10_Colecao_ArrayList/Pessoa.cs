4f8740c86efb1aa3418cddcf7c847a7783bb432f
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 10:26:09 +0000

    Update project D10.

--- D10_Colecao_ArrayList/Pessoa.cs
+++ D10_Colecao_ArrayList/Pessoa.cs
@@ -1,49 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace D10_Colecao_ArrayList;
 
-namespace D10_Colecao_ArrayList
+public class Pessoa
 {
+    
+    #region Properties
 
-    // Base class
-    internal class Pessoa
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    #endregion
+
+    #region Constructors (no mínimo 2)
+
+    public Pessoa()
     {
 
-        #region Properties
-
-        public int Id { get; set; }
-        public string Nome { get; set; }
-
-        #endregion
-
-        #region Constructors (no mínimo 2)
-
-        public Pessoa()
-        {
-
-            Id = 0;
-            Nome = string.Empty;
-
-        }
-
-        public Pessoa(int id, string nome)
-        {
-
-            Id = id;
-            Nome = nome;
-
-        }
-
-        #endregion
-
-        #region Methods
-
-
-
-        #endregion
+        Id = 0;
+        Nome = string.Empty;
 
     }
+
+    public Pessoa(int id, string nome)
+    {
+
+        Id = id;
+        Nome = nome;
+
+    }
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
 
 }
