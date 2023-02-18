180b9bb016904b7229d72f5028059caa52f82ed4
Leandro Gabriel <leandro@bigiogabriel.com>
2023-02-18 10:43:08 +0000

    Update project D18.

--- D18_LINQ/Program.cs
+++ D18_LINQ/Program.cs
@@ -1,95 +1,75 @@
-﻿//https://docs.microsoft.com/en-us/dotnet/csharp/linq/write-linq-queries
+﻿using D00_Utils;
 
-using D00_Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+Utils.PrintHeader("LINQ");
 
-namespace D18_LINQ
+#region Query Syntax
+
+Utils.PrintSubHeader("Query Syntax");
+
+List<int> numbers = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+// Query #1.
+// IEnumerable --> interface
+IEnumerable<int> filteringQuery =
+    from num in numbers
+    where num < 3 || num >= 7
+    select num;
+
+foreach (var item in filteringQuery)
 {
-
-    class Program
-    {
-
-        static void Main(string[] args)
-        {
-
-            Utils4.PrintHeader("LINQ");
-
-            #region Query Syntax
-
-            Utils4.PrintSubHeader("Query Syntax");
-
-            List<int> numbers = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
-            // Query #1.
-            // IEnumerable --> interface
-            IEnumerable<int> filteringQuery =
-                from num in numbers
-                where num < 3 || num >= 7
-                select num;
-
-            foreach (var item in filteringQuery)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine();
-
-            // Query #2.
-            var orderingQuery =
-                from num in numbers
-                where num < 3 || num > 7
-                orderby num ascending
-                select num;
-
-            foreach (var item in orderingQuery)
-            {
-                Console.WriteLine(item);
-            }
-            
-            Utils4.CleanConsole();
-
-            #endregion
-
-            #region Method Syntax
-
-            Utils4.PrintSubHeader("Method Syntax");
-
-            List<int> numbers1 = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-            List<int> numbers2 = new List<int>() { 15, 14, 11, 13, 19, 18, 16, 17, 12, 10 };
-
-            // Query #3.
-            double average = numbers1.Average();
-
-            Console.WriteLine(average);
-
-            Console.WriteLine();
-
-            // Query #4.
-            IEnumerable<int> numbersConcatenated = numbers1.Concat(numbers2);
-
-            foreach (var item in numbersConcatenated)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine();
-
-            // Query #5.
-            var numbersGreaterThan15 = numbers2.Where(c => c > 15).OrderBy(c => c);
-
-            foreach (var item in numbersGreaterThan15)
-            {
-                Console.WriteLine(item);
-            }
-
-            Utils4.CleanConsole();
-
-            #endregion
-            
-        }
-
-    }
-
+    Console.WriteLine(item);
 }
+
+Console.WriteLine();
+
+// Query #2.
+var orderingQuery =
+    from num in numbers
+    where num < 3 || num > 7
+    orderby num ascending
+    select num;
+
+foreach (var item in orderingQuery)
+{
+    Console.WriteLine(item);
+}
+            
+Utils.CleanConsole();
+
+#endregion
+
+#region Method Syntax
+
+Utils.PrintSubHeader("Method Syntax");
+
+List<int> numbers1 = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+List<int> numbers2 = new List<int>() { 15, 14, 11, 13, 19, 18, 16, 17, 12, 10 };
+
+// Query #3.
+double average = numbers1.Average();
+
+Console.WriteLine(average);
+
+Console.WriteLine();
+
+// Query #4.
+IEnumerable<int> numbersConcatenated = numbers1.Concat(numbers2);
+
+foreach (var item in numbersConcatenated)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+
+// Query #5.
+var numbersGreaterThan15 = numbers2.Where(c => c > 15).OrderBy(c => c);
+
+foreach (var item in numbersGreaterThan15)
+{
+    Console.WriteLine(item);
+}
+
+Utils.CleanConsole();
+
+#endregion
