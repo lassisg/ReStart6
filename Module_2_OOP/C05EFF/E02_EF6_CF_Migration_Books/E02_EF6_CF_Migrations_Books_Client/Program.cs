21ac59712a697d2fe0de4e2ad76173d68ace14af
Leandro Gabriel <lgabriel@growin.com>
2023-07-14 16:07:12 +0100

    Add E02_EF6_CF_Migrations_Books to C05EFF

---
+++ Module_2_OOP/C05EFF/E02_EF6_CF_Migration_Books/E02_EF6_CF_Migrations_Books_Client/Program.cs
@@ -0,0 +1,8 @@
+﻿using E02_EF6_CF_Migrations_Books_Client;
+
+PublishersDBClient.CreatePublisher();
+BooksDBClient.CreateBook();
+
+Console.WriteLine("Data entered successfully.");
+
+Console.ReadKey();
---
+++ Module_2_OOP/C05EFF/E02_EF6_CF_Migration_Books/E02_EF6_CF_Migrations_Books_DAL/Model/BooksDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E02_EF6_CF_Migrations_Books_DAL
+{
+
+    public class BooksDBContext : DbContext
+    {
+
+        public BooksDBContext()
+        {
+        }
+    
+        public BooksDBContext(DbContextOptions<BooksDBContext> options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibraryDB");
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        public DbSet<Book> Book { get; set; }
+
+    }
+
+}
