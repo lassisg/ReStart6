d9b1357fff5c5f9f5793f3c1f01d67a65287113f
Leandro Gabriel <lgabriel@growin.com>
2023-07-14 09:19:38 +0100

    Move D04_OOP_Abstraction to C04PCSA

---
+++ Module_2_OOP/C04PCSA/D04_OOP_Abstraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using D00_Utils;
+
+namespace D04_OOP_Abstraction;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Utils.SetUtf8Encoding();
+
+        Utils.PrintHeader("Abstraction");
+
+        // Não se pode instanciar uma classe abstract
+        //Course course = new Course();
+
+        CourseSoftSkills curso01 = new CourseSoftSkills(1, "Javascript Web Developer", "Dev", "Top");
+
+        curso01.ListCourse();
+
+        Utils.CleanConsole();
+
+    }
+
+}
