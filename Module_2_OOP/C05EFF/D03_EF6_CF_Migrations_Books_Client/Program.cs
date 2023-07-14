1c88b2acfda8bc320c62e27d0241d96ef0cbb983
Leandro Gabriel <lgabriel@growin.com>
2023-07-14 15:31:04 +0100

    D03_EF6_CF_Migrations_Books_Client to C05EFF

---
+++ Module_2_OOP/C05EFF/D03_EF6_CF_Migrations_Books_Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using D03_EF6_CF_Migrations_Books_Client;
+
+for (int i = 0; i < 3; i++)
+    PublisherRepository.CreatePublisher();
+
+BookRepository.CreateBook();
+
+Utility.WriteTitle("CREATE", "", "\n");
+PublisherRepository.ReadPublisher();
+BookRepository.ReadBook();
+
+Utility.WriteTitle("UPDATE", "\n\n\n", "\n");
+PublisherRepository.UpdatePublisher();
+PublisherRepository.ReadPublisher();
+BookRepository.ReadBook();
+
+Utility.WriteTitle("DELETE", "\n\n\n", "\n");
+PublisherRepository.DeletePublisher();
+PublisherRepository.ReadPublisher();
+BookRepository.ReadBook();
+
+Console.ReadKey();
