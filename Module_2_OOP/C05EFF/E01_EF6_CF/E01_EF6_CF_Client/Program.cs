1bbe47deba19d269ac3af79de8e9f1a820ed958e
Leandro Gabriel <lgabriel@growin.com>
2023-07-14 15:54:52 +0100

    Add E01_EF6_CF to C05EFF

---
+++ Module_2_OOP/C05EFF/E01_EF6_CF/E01_EF6_CF_Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using E01_EF6_CF_Client;
+using E01_EF6_CF_DAL;
+
+bool exitApplication = false;
+string userInput;
+
+do
+{
+    try
+    {
+
+        Utils.WriteMenu();
+        userInput = Console.ReadLine();
+
+        Console.Clear();
+
+        switch (userInput)
+        {
+            case "":
+                exitApplication = true;
+                break;
+
+            case "1":
+                Utils.WriteHeader("Adicionar livro (Enter para sair)");
+                Utils.GetBookDataFromUser()
+                     .ValidateBook()
+                     .Create()
+                     .WriteBookFeedbackMessage();
+
+                break;
+
+            case "2":
+                Utils.WriteHeader("Adicionar editora (Enter para sair)");
+                Utils.GetPublisherDataFromUser()
+                     .ValidatePublisher()
+                     .Create()
+                     .WritePublisherFeedbackMessage();
+
+                break;
+
+            case "3":
+                Utils.WriteHeader("Lista de livros");
+                BookRepository.GetAllBooks()
+                              .OrderBy(b => b.BookId)
+                              .ToList()
+                              .ForEach(b => Console.WriteLine(b.GetFormattedBook()));
+
+                break;
+
+            case "4":
+                Utils.WriteHeader("Lista de editoras");
+                PublisherRepository.GetAllPublishers()
+                                   .OrderBy(p => p.PublisherId)
+                                   .ToList()
+                                   .ForEach(p => Console.WriteLine($"{p.GetFormattedPublisher()}"));
+
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+        }
+
+        Console.WriteLine();
+
+
+    }
+    catch (Exception e)
+    {
+        Console.Clear();
+        Console.WriteLine($"Ocorreu um erro!\n{e.Message}\n");
+    }
+
+} while (!exitApplication);
+
+Console.Write("Até logo...");
+Console.ReadLine();
---
+++ Module_2_OOP/C05EFF/E01_EF6_CF/E01_EF6_CF_DAL/DbContext/LibraryContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E01_EF6_CF_DAL;
+
+public class LibraryContext : DbContext
+{
+    public LibraryContext()
+    {
+    }
+    
+    public LibraryContext(DbContextOptions<LibraryContext> options) 
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibraryDB_EF6CodeFirst");
+
+
+    public DbSet<Publisher> Publisher { get; set; }
+
+    public DbSet<Book> Book { get; set; }
+
+}
